d3d0d9c89d95596e0bdd7221144c490d4c10d298
FriendSK <49130sk@gmail.com>
2019-05-23 18:45:02 +0300

    'createGallery'

---
+++ src/components/pages/Gallery/reducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+    photos: [
+        { id:1, name: 'image-1' },
+        { id:2, name: 'image-2' },
+        { id:3, name: 'image-3' },
+        { id:4, name: 'image-4' },
+    ],
+}
+
+export default function (state = initialState, action) {
+
+    switch(action.type){
+        case 'ADD_IMAGE': 
+            return {
+                ...state,
+                photos: [...state.photos, action.image]
+            }
+        default: return state;
+    }    
+}
